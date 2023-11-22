a415afd242ecd0b8dce66515a04321e214f175db
daniel <xzarsk04@stud.fit.vutbr.cz>
2023-11-22 23:47:04 +0100

    feat: facility instead of store, skier decision logic

--- main.cpp
+++ main.cpp
@@ -1,61 +1,134 @@
 #include "simlib.h"
 
 ///time to lift the skier uphill
-const int way_up =  20;
+const int way_up_marta1 =  20;
+const int way_up_marta2 =  30;
+const int way_up_kotva =  25;
+const int departure =  10;
+
 
 ///first lift
-bool Marta1_seat_1_free = true;
-class Facility Marta1_seat_1("marta 1 seat 1");
 
-bool Marta1_seat_2_free = true;
-class Facility Marta1_seat_2("marta 1 seat 2");
+Store marta1(2);
+Store marta2(4);
+Store kotva(2);
 
+enum lift {
+    MARTA1,    // 0
+    MARTA2,  // 1
+    KOTVA    // 2
+};
 
-class Facility Marta2("marta 2");
-class Facility Kotva("kotva");
+bool lift_ready = false;
+bool ok = true;
+bool open = false;
 
-
-
+class Openning_hours: public Event{
+  void Behavior(){
+      open = true;
+      Activate(3000);
+      open = false;
+      Cancel();
+  }
+};
 ///skier
 class Skier : public Process {
     // double max_time;
     // bool pauzicka
     // ski vs board
-    int current_marta1 = 0;
-    void seize_marta1(){
-        if(Marta1_seat_1_free){
-            current_marta1 = 1;
-            Marta1_seat_1_free = false;
-            Seize(Marta1_seat_1);
+
+    int jizdy = 3;
+    lift current_lift;
+
+    void choose_start(){
+        int val = Random();
+        if(val < 0.33){
+            Print("Entered MARTA1");
+            current_lift = MARTA1;
+        }
+        else if(val < 0.66){
+            Print("Entered MARTA2");
+            current_lift = MARTA2;
         }
         else{
-            current_marta1 = 2;
-            Marta1_seat_2_free = false;
-            Seize(Marta1_seat_2);
+            Print("Entered KOTVA");
+            current_lift = KOTVA;
         }
     }
-    void release_marta1(){
-        if(current_marta1 == 1){
-            current_marta1 = 0;
-            Release(Marta1_seat_1);
-            Marta1_seat_1_free = true;
 
+    void ride_down(){
+        if(current_lift == MARTA1 || current_lift == KOTVA){// vyjel uplne nahoru
+          double val = Random();
+            if(val < 0.33){
+                Wait(108);
+                current_lift = KOTVA;
+            }
+            else if(val < 0.66){
+                Wait(108);
+                current_lift = MARTA2;
+            }
+            else{ //slunecna
+                Wait(36);
+                val = Random();
+                if (val < 0.5){
+                    Wait(108); // sjizdi marta 2
+                    current_lift = MARTA2;
 
-        } else{
-            current_marta1 = 0;
-            Seize(Marta1_seat_2);
-            Marta1_seat_2_free = true;
+                }else{
+                    Wait(168); //prejezd marta 2 -> 1
+                    current_lift = MARTA1;
+                }
+
+            }
+
+        }else{ //vyjel do pulky pres marta 2
+            double val = Random();
+            if (val < 0.5){
+                Wait(108); // sjizdi marta 2
+                current_lift = MARTA2;
+            }else{
+                Wait(168); //prejezd marta 2 -> 1
+                current_lift = MARTA1;
+            }
+
+        }
+
+    }
+    
+    void ride_up(){
+        
+        if(current_lift == MARTA1){
+            Enter(marta1, 1);
+            Wait(departure);
+            Leave(marta1, 1);
+            Wait(way_up_marta1);
+        }
+        else if (current_lift == MARTA2){
+            Enter(marta2, 1);
+            Wait(departure);
+            Leave(marta2, 1);
+            Wait(way_up_marta2);
+        }
+        else {
+            Enter(kotva, 1);
+            Wait(departure);
+            Leave(kotva, 1);
+            Wait(way_up_kotva);
 
         }
     }
-    void Behavior(){
-       Print("Skier arrived.\n");
+
+    void Behavior() override{
+       Print("Skier generated.\n");
        Print(Time);
-       seize_marta1();
-       Wait(way_up);
-       release_marta1();
-       //random rozhodovani
-       Print("Skier left the lift.\n");
+       
+       choose_start();
+       while(jizdy > 0){
+               ride_up();
+               ride_down();
+               jizdy --;
+       }
+       Print("Skier leaving the system.\n");
        Print(Time);
     }
 };
@@ -64,24 +137,31 @@ class Generator : public Event{
     int skier_cnt = 0;
     // nocni rezim
     //dennin rezim
-    void Behavior(){
+    void Behavior() override{
         Print(skier_cnt);
         (new Skier)->Activate();
         skier_cnt ++;
-        Activate(Time + Exponential(50));
+        Activate(Time + Exponential(5));
     }
 };
 
 int main(int argc, char* argv[]){
 
-
     Print("Project IMS 2023");
     Init(0, 1000);
+    (new Openning_hours)->Activate();
+
     (new Generator)->Activate();
+
     Run();
     Print("Simulation done.");
-    Marta1_seat_1.Output();
-    Marta1_seat_2.Output();
+    Print("Marta 1");
+    marta1.Output();
+    Print("Marta 2");
+    marta2.Output();
+    Print("kotva");
+    kotva.Output();
+
 
     return 0;
 }
